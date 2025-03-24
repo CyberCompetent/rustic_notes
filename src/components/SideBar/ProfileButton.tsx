8a30efe54ee08a6180471dabfdafeb066bb2b069
Nathan van Niekerk <nathvn3@protonmail.com>
2025-03-24 14:43:07 +0000

    Added navigate to login page when Logout is clicked

--- src/components/SideBar/ProfileButton.tsx
+++ src/components/SideBar/ProfileButton.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import { useSettings } from '@/context/SettingsContext'; // Adjust the path based on your file structure
 import Dropdown from '@/components/templates/Dropdown-button'; // Import the Dropdown component
 
 const ProfileButton: React.FC = () => {
   const { openSettings } = useSettings();
+  const navigate = useNavigate();
 
   const options = ['Logout', 'Manage Workspaces'];
 
   const handleSelect = (option: string) => {
     switch (option) {
       case "Logout":
-        console.log("Logging out...");
+        navigate("/");
         break;
       case "Manage Workspaces":
         openSettings("Workspaces");
